ad28f58a3abf125bbcda44cdcdf6a4c1d86ff5ee
Andrey Karabeshkin <karabeshkin.a@mail.ru>
2023-04-21 15:56:53 +0300

    pochti final

--- controller.js
+++ controller.js
@@ -1,6 +1,8 @@
 const readlineSync = require('readline-sync'); 
-const model = require('model()');
-const check = 
+// const model = require('model()');
+const {
+  model
+} = require('./index');
 
 
 // вывод тем
@@ -21,10 +23,29 @@ function communicator() {
   const choosingTheme = readlineSync.question('Chose thr theme: ');
   console.log('You choosed theme:  ' + choosingTheme + '!');
   const arr = model(choosingTheme)
-  for (let i = 0; i < arr.lentgth; i += 1) {
+  console.log(arr);
+  // const choosingTheme = readlineSync.question(' ')
+  for (let i = 0; i < arr.length; i += 1) {
     console.log(arr[i].question)
     const otvet = readlineSync.question('otvet: ');
-    console.log(check(otvet))
+    if (arr[i].answer === otvet) {
+      console.log(arr[i].answer);
+      console.log(otvet);
+
+      console.log('Verno')
+    } else {
+      console.log(arr[i].answer);
+      console.log(otvet);
+      console.log('Neverno');
+    }
+    // arr.find((el)=> {
+    //   if (el.answer === otvet) {
+    //     console.log("Верно"); 
+    //   } else {
+    //     console.log("неВерно");
+    //   }
+  // })
+    // console.log(check(otvet))
   }
   const bye = 'Вопросы кончились';
   return bye
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const themes = fs.readdirSync('./themes');
 function model(znachenie) {
 // console.log(themes);
   const quetions = fs.readFileSync(`./themes/${themes[znachenie]}/questions.txt`, 'utf-8').split('\n');
-//   console.log(quetions);
+// console.log(quetions);
   const answers = fs.readFileSync(`./themes/${themes[znachenie]}/answers.txt`, 'utf-8').split('\n');
   const arr = quetions.map((el, i) => {
     const obj = {};
@@ -13,13 +13,20 @@ function model(znachenie) {
     obj.answer = answers[i];
     return obj;
   });
+  return arr;
 }
 // const per = readlineSync.question('')
 // console.log(arr);
-function check(elem) {
-  const done = arr.filter(((el, i) => (el[znachenie].answer).toLowerCase() === per).toLowerCase());
-  if (!done) {
-    return console.log('Верно!');
-  }
-  return console.log(('Неверно!'));
-}
+// function check(elem) {
+//   const done = arr.filter(((el, i) => (el[znachenie].answer).toLowerCase() === per).toLowerCase());
+//   if (!done) {
+//     return console.log('Верно!');
+//   }
+//   return console.log(('Неверно!'));
+// }
+
+
+module.exports = { model };
+// module.exports = check;
+
+// module.export = model; 
